452419d6f0ddcfeec47d89773472eac344856dcc
conao3 <conao3@gmail.com>
2020-02-13 01:12:29 +0900

    add Package-Requires specification

--- remind-bindings.el
+++ remind-bindings.el
@@ -5,7 +5,7 @@
 ;; Author: Mehmet Tekman
 ;; URL: https://github.com/mtekman/remind-bindings.el
 ;; Keywords: outlines
-;; Package-Requires: ((emacs "25.1"))
+;; Package-Requires: ((emacs "25.1") (omni-quotes "0.5") (popwin "1.0"½))
 ;; Version: 0.6
 
 ;;; Commentary:
